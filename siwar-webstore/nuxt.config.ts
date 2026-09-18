e16e345eebaf016f27cf3631ff79897344b91a85
ibrahim alzoubi <gusalzib@student.gu.se>
2026-09-18 10:48:47 +0200

    #1 [Config] Configure i18n and add locales files for ar, sv, and en

--- siwar-webstore/nuxt.config.ts
+++ siwar-webstore/nuxt.config.ts
@@ -1,5 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  typescript: {
+    strict: true,
+    typeCheck: true,
+  },
+
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt',
+    '@nuxtjs/i18n',
+  ],
+
+  i18n: {
+    langDir: 'i18n/locales/',
+    defaultLocale: 'ar',
+    strategy: 'prefix_and_default',
+    locales: [
+      { code: 'ar', iso: 'ar-SA', file: 'ar.json', name: 'العربية', dir: 'rtl' },
+      { code: 'en', iso: 'en-US', file: 'en.json', name: 'English', dir: 'ltr' },
+      { code: 'sv', iso: 'sv-SE', file: 'sv.json', name: 'Svenska', dir: 'ltr' },
+    ]
+  },
 })
